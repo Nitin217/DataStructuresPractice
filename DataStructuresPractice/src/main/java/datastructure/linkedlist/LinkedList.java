9b0e16d417d610589df5b76ae7db49ebb64cf88e
Nitin Behl <nitinbehl2@gmail.com>
2020-09-18 23:59:26 -0400

    Add insert and remove method to linkedlist

--- DataStructuresPractice/src/main/java/datastructure/linkedlist/LinkedList.java
+++ DataStructuresPractice/src/main/java/datastructure/linkedlist/LinkedList.java
@@ -1,5 +1,7 @@
 package main.java.datastructure.linkedlist;
 
+import java.util.Arrays;
+
 import lombok.Data;
 
 @Data
@@ -27,15 +29,67 @@ public class LinkedList {
 		Node newNode= new Node(value);
 		newNode.setNext(this.head);
 		this.head=newNode;
+		this.length++;
 	}
 	
 	public void printList() {
+		int[] list=new int[this.length];
 		Node currentNode = this.head;
+		int i=0;
 		while (currentNode != null) {
-			System.out.println(currentNode.getValue());
+			list[i]=currentNode.getValue();
 			currentNode = currentNode.getNext();
+			i++;
+		}
+		System.out.println(Arrays.toString(list));
+	}
+	
+	public void insert(int node, int value) {
+		// if node is greater than length , throw error
+		if (node >= this.length) {
+			this.append(value);
+		} else {
+			// if node is less than or equal to length then insert
+			int i = 0;
+			Node currentNode = this.head;
+			while (i <= node) {
+				if (i < node) {
+					currentNode = currentNode.getNext();
+
+				} else if (i == node) {
+					Node newNode = new Node(currentNode.getValue());
+					newNode.setNext(currentNode.getNext());
+					currentNode.setValue(value);
+					currentNode.setNext(newNode);
+					this.length++;//forgot to increase length
+				}
+				i++;
+			}
 		}
 
+	}
+	
+	public void remove(int node) {
+		if(node>=this.length || node<0) {
+			System.out.println("Invalid Index");
+		}else {
+			int i=0;
+			Node leaderNode = this.head;
+			Node nodeTodelete;
+		
+			while(i<=node) {
+				if (i < node-1) {
+					leaderNode = leaderNode.getNext();
+
+				}else if (i == node) {
+					nodeTodelete = leaderNode.getNext();
+					leaderNode.setNext(nodeTodelete.getNext());
+					this.length--;
+				}
+				i++;
+			}
+		}
+		
 	}
 	
 	
@@ -44,7 +98,10 @@ public class LinkedList {
 		list.append(5);
 		list.append(16);
 		list.prepend(1);
-		System.out.println(list.toString());
+		list.insert(66, 2);;
+		list.insert(1, 100);;
+		list.printList();
+		list.remove(6);
 		list.printList();
 	}
 }
